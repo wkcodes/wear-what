aba4cfbf950c0b980017204ad452050c4b06b899
wkcodes <wkcodes2020@gmail.com>
2021-08-26 09:41:40 -0700

    integrated fetch weather data call into WearBox component

--- src/components/WearBox.vue
+++ src/components/WearBox.vue
@@ -2,24 +2,24 @@
     <div class="container">
       <button>toggle time of day</button>
       <img src="../../public/img/clothes/short_sleeve_shirt.png" alt="pants" width="150" height="150">
-      <p>{{message}}</p>
+      <p>It's {{currentWeather}} in {{city}}</p>
     </div>
 </template>
 
 <script>
-  import WeatherHandler from '../API/WeatherHandler.js'
-
-  let data = WeatherHandler()
-
-  console.log(data)
-
   export default {
     name: 'WearBox',
     data() {
       return {
-        message: 'Its (cool, hot etc.)'
+        city: 'Seattle',
+        currentWeather: {}
       }
-    } 
+    }, 
+    mounted() {
+      fetch('https://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=da643a7d6952012ad2356e9fe073f685')
+        .then(res => res.json())
+        .then(data => this.currentWeather = data.main.temp)
+    }
   };
 </script>
 
